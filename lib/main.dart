69943eb70a19db028d7d671aa7d2b9728587688e
juanmarsilva <juanmarsilva>
2025-06-20 12:01:24 -0300

    configure theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:riverpod_app/config/config.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      theme: AppTheme().getTheme(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
